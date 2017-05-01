61392e356967590d2e4d483ebf71965449f72f86
Javier Cabezas <jcleyton@gmail.com>
2017-05-01 00:31:47 -0300

    Icon scrolling

--- section04_ScrollUIView/scrollview/ViewController.swift
+++ section04_ScrollUIView/scrollview/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [ UIImageView]()
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            self.images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            //First loop X = mid, second loop X = 3/2 mid, etc..
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: view.frame.size.height / 2 - 75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
