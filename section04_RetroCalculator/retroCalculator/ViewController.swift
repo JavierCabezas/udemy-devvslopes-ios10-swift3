6cca893efedecf3387c1aa5d68bb932b36a4814f
Javier Cabezas <jcleyton@gmail.com>
2017-05-06 18:33:42 -0300

    Retro calculator

--- section04_RetroCalculator/retroCalculator/ViewController.swift
+++ section04_RetroCalculator/retroCalculator/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLbl: UILabel!
+
     var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    var currentOperation = Operation.Empty
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +43,24 @@ class ViewController: UIViewController {
     
     @IBAction func numberPressed(sender: UIButton){
         playSound()
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func operatorPressed(sender: UIButton){
+        switch sender.tag{
+            case 10:
+                processOperation(operation: .Add)
+            case 11:
+                processOperation(operation: .Subtract)
+            case 12:
+                processOperation(operation: .Divide)
+            case 13:
+                processOperation(operation: .Multiply)
+            default:
+                currentOperation = Operation.Empty
+                processOperation(operation: currentOperation)
+        }
     }
     
     func playSound(){
@@ -35,6 +68,36 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
+    }
+    
+    func processOperation(operation: Operation){
+        playSound()
+        if currentOperation != Operation.Empty {
+            if runningNumber != ""{
+                rightValStr = runningNumber
+            }
+            runningNumber = ""
+            
+            switch currentOperation {
+                case Operation.Multiply:
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                case Operation.Add:
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                case Operation.Divide:
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                case Operation.Subtract:
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                default:
+                    print ("!")
+            }
+            
+            leftValStr = result
+            outputLbl.text = result
+        } else{
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
     }
 }
 
