f3c5088166a2860962085a7ca40d12685c95a443
Javier Cabezas <jcleyton@gmail.com>
2017-05-14 21:44:08 -0400

    Added main interface

---
+++ section04_PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Javier Cabezas Leyton on 5/8/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
+
