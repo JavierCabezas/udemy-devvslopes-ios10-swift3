33091a61644c55280a4e9464ac61f2a8ea1bd698
Javier Cabezas <jcleyton@gmail.com>
2017-05-15 23:23:00 -0400

    Created view and model files for the party rock app

---
+++ section04_PartyRockApp/PartyRockApp/PartyCell.swift
@@ -0,0 +1,25 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Javier Cabezas Leyton on 5/15/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func updateUI(){
+        
+    }
+}
---
+++ section04_PartyRockApp/PartyRockApp/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRockApp
+//
+//  Created by Javier Cabezas Leyton on 5/15/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import Foundation
