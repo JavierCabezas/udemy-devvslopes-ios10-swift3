e603c7cede492efa708eb59b9a6807286ab578f7
Javier Cabezas <jcleyton@gmail.com>
2017-05-21 11:34:08 -0400

    Constant file and CurrentWeather class

---
+++ section05_RainyShinyCloudy/rainyshinycloudy/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  rainyshinycloudy
+//
+//  Created by Javier Cabezas Leyton on 5/21/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://samples.openweathermap.org/data/2.5/weather"
+let LAT = "lat="
+let LONG = "long="
+let APP_ID = "appid="
+let API_KEY = "0e955a2338ef1ce44b5cd29cc0e44fbb"
+
+let CURRENT_URL = "\(BASE_URL)?\(LAT)-36&\(LONG)123&\(APP_ID)\(API_KEY)"
---
+++ section05_RainyShinyCloudy/rainyshinycloudy/CurrentWeather.swift
@@ -0,0 +1,41 @@
+//
+//  CurrentWeather.swift
+//  rainyshinycloudy
+//
+//  Created by Javier Cabezas Leyton on 5/21/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+
+    var cityName: String{
+        return _cityName == nil ? "" : _cityName
+    }
+    
+    var weatherType: String {
+        return _weatherType == nil ? "" : _weatherType
+    }
+    
+    var currentTemp: Double {
+        return _currentTemp == nil ? 0.0 : _currentTemp
+    }
+    
+    var date: String {
+        if _date == nil {
+            return ""
+        }
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        return "Today, " + dateFormatter.string(from: Date())
+    }
+    
+}
