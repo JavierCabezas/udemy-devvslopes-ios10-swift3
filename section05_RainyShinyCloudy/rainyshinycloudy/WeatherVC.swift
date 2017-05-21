6cb6478a0738ce2659163c24fdfba82ed287c795
Javier Cabezas <jcleyton@gmail.com>
2017-05-20 22:46:37 -0400

    Implemented tableview functions

--- section05_RainyShinyCloudy/rainyshinycloudy/WeatherVC.swift
+++ section05_RainyShinyCloudy/rainyshinycloudy/WeatherVC.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class WeatherWC: UIViewController {
-
+class WeatherWC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
 }
 
