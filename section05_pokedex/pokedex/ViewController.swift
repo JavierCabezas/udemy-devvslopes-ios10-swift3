39251650c34e0ec514433f7f9f1eff183feb2f9d
Javier Cabezas <jcleyton@gmail.com>
2017-06-04 11:03:07 -0400

    Filling pokécells with actual (hardcoded) pokémon data

--- section05_pokedex/pokedex/ViewController.swift
+++ section05_pokedex/pokedex/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection :UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collection.dataSource = self
+        collection.delegate = self
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "temp", pokedexId: indexPath.row + 1)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 27
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
 }
 
