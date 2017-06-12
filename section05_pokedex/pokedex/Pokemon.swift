4bff12ff0224a8ca03070ba96171dbffc3eac82d
Javier Cabezas <jcleyton@gmail.com>
2017-06-12 07:30:42 -0400

    Added variables on pokémon model to store extra information

--- section05_pokedex/pokedex/Pokemon.swift
+++ section05_pokedex/pokedex/Pokemon.swift
@@ -11,6 +11,14 @@ import Foundation
 class Pokemon {
     private var _name:String!
     private var _pokedexId:Int!
+    private var _description:String!
+    private var _type:String!
+    private var _defense:String!
+    private var _height:String!
+    private var _attack:String!
+    private var _weigth:String!
+    private var _nextEvolutionTxt:String!
+    private var _nextEvolutionId:Int!
     
     var name: String {
         return _name
@@ -18,6 +26,37 @@ class Pokemon {
     
     var pokedexId: Int {
         return _pokedexId
+    }
+    
+    var description:String {
+        return _description
+    }
+    
+    var type:String {
+        return _type
+    }
+    
+    var defense:String {
+        return _defense
+    }
+    
+    var height:String {
+        return _height
+    }
+    
+    var attack:String {
+        return _attack
+    }
+    
+    var weigth:String {
+        return _weigth
+    }
+    var nextEvolutionTxt:String {
+        return _nextEvolutionTxt
+    }
+    
+    var nextEvolutionId:Int {
+        return _nextEvolutionId
     }
     
     init(name: String, pokedexId: Int){
