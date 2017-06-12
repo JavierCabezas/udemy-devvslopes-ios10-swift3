a0e7922190416084765c95fd8d21db13c36124f6
Javier Cabezas <jcleyton@gmail.com>
2017-06-12 07:24:41 -0400

    Updated back button with dismiss instead of segue

--- section05_pokedex/pokedex/PokemonDetailVC.swift
+++ section05_pokedex/pokedex/PokemonDetailVC.swift
@@ -12,6 +12,16 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var pokemonPictureImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var numberLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var atkLbl: UIStackView!
+    @IBOutlet weak var defLbl: UILabel!
+    @IBOutlet weak var nextEvolutionLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +32,7 @@ class PokemonDetailVC: UIViewController {
     }
     
     @IBAction func onBackClick(_ sender: Any) {
-        performSegue(withIdentifier: "PokemonListVC", sender: nil)
+        dismiss(animated: true, completion: nil)
     }
     
 
