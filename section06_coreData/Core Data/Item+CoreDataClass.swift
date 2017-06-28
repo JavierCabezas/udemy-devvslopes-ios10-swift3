d66467d64e53cbdb8c44f17a088ce64c1199ded2
Javier Cabezas <jcleyton@gmail.com>
2017-06-28 00:20:06 -0400

    Created main data model

---
+++ section06_coreData/Core Data/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  Core Data
+//
+//  Created by Javier Cabezas Leyton on 6/28/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
