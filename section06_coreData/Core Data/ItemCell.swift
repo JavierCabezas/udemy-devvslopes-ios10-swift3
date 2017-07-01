7afe63d3ec6e3c27159f17f391b09f154e1b0c05
Javier Cabezas <jcleyton@gmail.com>
2017-07-01 19:47:50 -0400

    Added ItemCell model

---
+++ section06_coreData/Core Data/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Core Data
+//
+//  Created by Javier Cabezas Leyton on 7/1/17.
+//  Copyright © 2017 Javier Cabezas Leyton. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+   
+}
